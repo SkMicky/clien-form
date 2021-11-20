fc23f49ea59dfcb289b54eea409a2d0f6376c6e9
SkMicky <m.skripkin@alpha-safety.kz>
2021-11-21 01:20:56 +0600

    project, routing and layout created, header component developed. Project configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PagesModule} from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
