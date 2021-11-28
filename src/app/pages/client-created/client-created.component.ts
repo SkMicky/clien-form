acc6659b349a0d5d86de4ee985d2b16e5d24c504
SkMicky <m.skripkin@alpha-safety.kz>
2021-11-28 14:30:04 +0600

    client-created page finished

--- src/app/pages/client-created/client-created.component.ts
+++ src/app/pages/client-created/client-created.component.ts
@@ -2,6 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {UtilsService} from '../../services/utils.service';
 import {IClient} from '../../shared/models/client.model';
+import {DomSanitizer} from '@angular/platform-browser';
+import {generateRandomColor} from '../../shared/helpers/random-color-generator';
 
 @Component({
   selector: 'app-client-created',
@@ -13,32 +15,34 @@ export class ClientCreatedComponent implements OnInit, OnDestroy {
   client: any = {};
 
   documentFile: File;
+  documentSrc: any;
 
   private _sub: Subscription;
 
-  constructor(private _utilsService: UtilsService) { }
+  constructor(private _utilsService: UtilsService, private _sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this._sub = this._utilsService.clientDocumentFile.subscribe(data => {
-      console.log(data);
-      this.documentFile = data;
+      if (data) {
+        this.documentFile = data;
+        this.documentSrc = this._sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(this.documentFile));
+      }
     });
 
     this.client.personal = localStorage.getItem('clientInfo') ? JSON.parse(localStorage.getItem('clientInfo')) : null;
     this.client.address = localStorage.getItem('clientAddress') ? JSON.parse(localStorage.getItem('clientAddress')) : null;
     this.client.identity = localStorage.getItem('clientIdentity') ? JSON.parse(localStorage.getItem('clientIdentity')) : null;
-
-    console.log(this.client, localStorage.getItem('clientInfo'));
   }
 
   ngOnDestroy(): void {
-    localStorage.removeItem('clientInfo');
-    localStorage.removeItem('clientAddress');
-    localStorage.removeItem('clientIdentity');
     localStorage.clear();
 
     if (this._sub) {
       this._sub.unsubscribe();
+    }
+
+    if (this.documentSrc) {
+      URL.revokeObjectURL(this.documentSrc);
     }
   }
 
